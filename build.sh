6571de668f187ec78e8c84b0bff2c91b18f01a94
hagneva1 <hagneva1@dgs-13-15ab-1752.univ-rouen.fr>
2019-11-19 14:58:47 +0100

    ajust build.sh and esup-installer.properties from web app

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-file=resources/esup-installer.properties
-
-function getProperty {
-   PROP_KEY=$1
-   PROP_VALUE=`cat $file | grep "$PROP_KEY" | cut -d'=' -f2`
-   echo $PROP_VALUE
-}
-
 rm -rf esup-sgc-client
 rm -rf esup-nfc-tag-desktop
 rm -rf esup-nfc-tag-keyboard
@@ -16,10 +8,9 @@ git clone https://github.com/EsupPortail/esup-sgc-client.git
 git clone https://github.com/EsupPortail/esup-nfc-tag-desktop.git
 git clone https://github.com/EsupPortail/esup-nfc-tag-keyboard.git	   
 
-export JAVA_HOME=$(getProperty "JAVA_HOME")  
-sed -i -e "s&https://esup-sgc.univ-ville.fr&$(getProperty "esupSgcUrl")&g" esup-sgc-client/src/main/resources/esupsgcclient.properties
-sed -i -e "s&https://esup-nfc-tag.univ-ville.fr&$(getProperty "esupNfcTagServerUrl")&g" esup-sgc-client/src/main/resources/esupsgcclient.properties esup-nfc-tag-desktop/src/main/resources/esupnfctag.properties esup-nfc-tag-keyboard/src/main/resources/esupnfctagkeyboard.properties
-sed -i -e "s&https://esup-sgc.univ-ville.fr/manager/{0}&$(getProperty "redirectUrlTemplate")&g" esup-nfc-tag-keyboard/src/main/resources/esupnfctagkeyboard.properties
+sed -i -e "s&https://esup-sgc.univ-ville.fr&$0&g" esup-sgc-client/src/main/resources/esupsgcclient.properties
+sed -i -e "s&https://esup-nfc-tag.univ-ville.fr&$0&g" esup-sgc-client/src/main/resources/esupsgcclient.properties esup-nfc-tag-desktop/src/main/resources/esupnfctag.properties esup-nfc-tag-keyboard/src/main/resources/esupnfctagkeyboard.properties
+sed -i -e "s&https://esup-sgc.univ-ville.fr/manager/{0}&$0/manager/{0}&g" esup-nfc-tag-keyboard/src/main/resources/esupnfctagkeyboard.properties
 
 mvn -f esup-sgc-client/pom.xml clean package
 mvn -f esup-nfc-tag-desktop/pom.xml clean package
