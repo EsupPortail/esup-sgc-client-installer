f26c0a18637c979620f1ff98ad2ee72dfb6c57be
hagneva1 <hagneva1@dgs-13-15ab-1752.univ-rouen.fr>
2019-11-25 16:32:00 +0100

    change name of zip archive

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ mvn -f esup-nfc-tag-desktop/pom.xml clean package
 mvn -f esup-nfc-tag-keyboard/pom.xml clean package
 mvn clean package
 
-7z a target/esupsgc-installer.7z ./target/esupsgc-installer.exe
-7z a target/esupsgc-installer.7z ./target/jdk
+7z a target/esup-sgc-client-installer.7z ./target/esupsgc-installer.exe
+7z a target/esup-sgc-client-installer.7z ./target/jdk
 
 cp esup-sgc-client/target/esup-sgc-client-final.jar target/esupsgcclient-shib.jar
 cp esup-nfc-tag-keyboard/target/esup-nfc-tag-keyboard-final.jar target/esupnfctagkeyboard.jar
